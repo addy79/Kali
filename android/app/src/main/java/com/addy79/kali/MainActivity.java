bc3257caf4aa518837cc6f00319d1941c8f50220
Kali Builder <dev@local>
2025-11-16 17:58:54 +0100

    Add Android (Capacitor) project and config

---
+++ android/app/src/main/java/com/addy79/kali/MainActivity.java
@@ -0,0 +1,5 @@
+package com.addy79.kali;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.addy79.kali',
+  appName: 'Kali Chess',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+}
+
+export default config
